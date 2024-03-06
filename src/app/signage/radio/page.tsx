04320131a54178ca1c73ba9c27bd5b91d3cb4512
mkaraki <26180919+mkaraki@users.noreply.github.com>
2024-03-07 04:04:23 +0900

    Add radio club content

---
+++ src/app/signage/radio/page.tsx
@@ -0,0 +1,9 @@
+import MoveExternalPage from "@/components/move-external-page";
+
+export default function JumpSignageRadio () {
+    return (
+        <>
+            <MoveExternalPage author="島根大学無線通信技術部" url="https://sites.google.com/view/ja4ype/home?utm_source=signage&utm_medium=display&utm_campaign=pim_signage" />
+        </>
+    );
+};
