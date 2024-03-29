d9e89debe4653563a21b7295367b0ff5c36f93eb
Sunny <122193933+Sunny-JP@users.noreply.github.com>
2024-03-29 16:45:07 +0900

    Add seminar club content

---
+++ src/app/signage/seminar/page.tsx
@@ -0,0 +1,9 @@
+import MoveExternalPage from "@/components/move-external-page";
+
+export default function JumpSignageSeminar () {
+    return (
+        <>
+            <MoveExternalPage author="島根大学セミナー部" url="https://sites.google.com/view/shimadai-seminar/%E3%83%9B%E3%83%BC%E3%83%A0?utm_source=signage&utm_medium=display&utm_campaign=pim_signage" />
+        </>
+    );
+};
