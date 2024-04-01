99792230e994729df2719c0ead7f80bfeae455a9
palalaw <106445721+palalaw@users.noreply.github.com>
2024-04-01 17:50:50 +0900

    セミナー部追加しました

---
+++ src/app/signage/seminar/page.tsx
@@ -0,0 +1,9 @@
+import MoveExternalPage from "@/components/move-external-page";
+
+export default function JumpSignageHoyoverse () {
+    return (
+        <>
+            <MoveExternalPage author="島根大学セミナー部" url="https://sites.google.com/view/shimadai-seminar/%E3%83%9B%E3%83%BC%E3%83%A0" />
+        </>
+    );
+};
