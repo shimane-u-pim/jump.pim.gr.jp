bc3ab3b16dc6d52bb039d2e348bf3b244aa29057
takut <takutaku3230gudakusan@gmail.com>
2024-04-16 16:40:43 +0900

    鐡道研究会を追加、関数名を修正した

--- src/app/signage/railway/page.tsx
+++ src/app/signage/railway/page.tsx
@@ -1,6 +1,6 @@
 import MoveExternalPage from "@/components/move-external-page";
 
-export default function JumpSignageRadio () {
+export default function JumpSignageRailway () {
     return (
         <>
             <MoveExternalPage author="島根大学鐡道研究会" url="https://twitter.com/shimadai_rail" />
